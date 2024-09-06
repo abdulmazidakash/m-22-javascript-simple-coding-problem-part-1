443aa7aff3d796c9e2bd4b2c9b722d4881550551
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-06 22:15:42 +0600

    22-1 js concepts recap and var, let and const

---
+++ recap.js
@@ -0,0 +1,19 @@
+/**
+ * variable: null
+ * undefined
+ * number
+ * string 
+ * boolean
+ * array
+ * object
+ * 
+ * condition
+ * array
+ * loop
+ * object
+ * function
+ * 
+ * 
+ * const
+ * let
+ */
