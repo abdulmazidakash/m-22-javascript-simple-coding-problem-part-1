877eb1278c8efee08efa0785b97f0486151bf004
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-09-07 09:50:43 +0600

    22-6 math, abs, round, ceil, floor, and random number

---
+++ math.js
@@ -0,0 +1,27 @@
+// const Math = {
+// 	min: function min(num1, num2) {
+// 		return num2;
+// 	}
+// }
+
+// console.log(Math.min);
+// const min = Math.min
+
+const min = Math.min(45, 34, 234, 23, 34, 456, -435);
+console.log(min);
+
+const max = Math.max(45, 34, 234, 23, 34, 456, -435);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(5-10));
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+console.log(Math.floor(2.99995));
+console.log(Math.ceil(2.99995));
+console.log(Math.ceil(2.00001));
+console.log(Math.random());;
+console.log(Math.random() * 10);
+
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
